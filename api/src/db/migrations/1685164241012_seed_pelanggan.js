8751f282451db287b74b7c0fbd0ac087e2b1f269
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-27 17:50:07 +0700

    ganti format nomor telepon pada seed pelanggan

--- api/src/db/migrations/1685164241012_seed_pelanggan.js
+++ api/src/db/migrations/1685164241012_seed_pelanggan.js
@@ -2,11 +2,11 @@ const { fakerUtils } = require('../../utils');
 
 module.exports = {
  up: `INSERT INTO pelanggan (nama_brand, jenis_usaha, no_telp) VALUES
- ('Brand A', 'Kafe', '${fakerUtils.phone.number('+628##########')}'),
- ('Brand B', 'Restoran', '${fakerUtils.phone.number('+628##########')}'),
- ('Brand C', 'Warung Makan', '${fakerUtils.phone.number('+628##########')}'),
- ('Brand D', 'Kafe', '${fakerUtils.phone.number('+628##########')}'),
- ('Brand E', 'Restoran', '${fakerUtils.phone.number('+628##########')}')`,
+ ('Brand A', 'Kafe', '${fakerUtils.phone.number('628##########')}'),
+ ('Brand B', 'Restoran', '${fakerUtils.phone.number('628##########')}'),
+ ('Brand C', 'Warung Makan', '${fakerUtils.phone.number('628##########')}'),
+ ('Brand D', 'Kafe', '${fakerUtils.phone.number('628##########')}'),
+ ('Brand E', 'Restoran', '${fakerUtils.phone.number('628##########')}')`,
 
  down: 'TRUNCATE pelanggan',
 };
