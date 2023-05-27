149b9e69eb864feda8c21b8dd97729a48ac1d515
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-27 19:13:19 +0700

    perbaiki file migration

--- api/src/db/migration.js
+++ api/src/db/migration.js
@@ -1,6 +1,14 @@
 const mysqlMigrations = require('mysql-migrations');
 
-const connection = require('./init');
+const mysql2 = require('mysql2');
+const { appConfig } = require('../config');
+
+const connection = mysql2.createPool({
+ host: appConfig.DB.HOST,
+ user: appConfig.DB.USER,
+ password: appConfig.DB.PASSWORD,
+ database: appConfig.DB.NAME,
+});
 
 mysqlMigrations.init(connection, __dirname + '/migrations', function () {}, [
  '--migrate-all',
