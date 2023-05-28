71686f3d4167c6f0bb672b83b0a2f411ab47df97
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 17:13:49 +0700

    update index controller penambahan controller pelanggan

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,9 +1,11 @@
 const registerController = require('./register.controller');
 const cabangController = require('./cabang.controller');
 const pesananController = require('./pesanan.controller');
+const pelangganController = require('./pelanggan.controller');
 
 module.exports = {
  registerController,
  cabangController,
  pesananController,
+ pelangganController,
 };
