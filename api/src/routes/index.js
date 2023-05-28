b658415d9268cbb79547242f54868859374825ee
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 17:18:07 +0700

    update index route penambahan route pelanggan

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,13 @@ const router = require('express').Router();
 const registerRouter = require('./register.route');
 const cabangRouter = require('./cabang.route');
 const pesananRouter = require('./pesanan.route');
+const pelangganRouter = require('./pelanggan.route');
 
-router.use('/api', [registerRouter, cabangRouter, pesananRouter]);
+router.use('/api', [
+ registerRouter,
+ cabangRouter,
+ pesananRouter,
+ pelangganRouter,
+]);
 
 module.exports = router;
