a91ec266456df7a285130abf98c818a559c127db
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 14:28:27 +0700

    update index models

--- api/src/models/index.js
+++ api/src/models/index.js
@@ -1,7 +1,13 @@
 const pelangganModel = require('./pelanggan');
 const cabangModel = require('./cabang');
+const produkModel = require('./produk');
+const pesananModel = require('./pesanan');
+const detailPesananModel = require('./detail_pesanan');
 
 module.exports = {
  pelangganModel,
  cabangModel,
+ produkModel,
+ pesananModel,
+ detailPesananModel,
 };
