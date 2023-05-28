82c7015ec7d4c7e0af523255b9dc3e502153f661
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 14:28:56 +0700

    update index routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const registerRouter = require('./register.route');
+const cabangRouter = require('./cabang.route');
+const pesananRouter = require('./pesanan.route');
 
-router.use('/api', [registerRouter]);
+router.use('/api', [registerRouter, cabangRouter, pesananRouter]);
 
 module.exports = router;
