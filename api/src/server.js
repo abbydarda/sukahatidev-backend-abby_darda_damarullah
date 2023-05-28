b211bc2395f5b93c2f72fe283471d308399be17a
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 11:48:05 +0700

    memperbaiki cara pemanggilan port pada config dan tambah pemanggilan route dan middleware errorHandler

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const routes = require('./routes');
+
 const app = express();
 const bodyParser = require('body-parser');
-const { appConfig } = require('./config');
-const PORT = appConfig.APP.PORT;
+const {
+ appConfig: { APP },
+} = require('./config');
+const {
+ errorHandlerMid: { errorHandler },
+} = require('./middlewares');
+const PORT = APP.PORT;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(routes);
+app.use(errorHandler);
 app.listen(PORT, () => {
  console.log(`Server berjalan di PORT ${PORT}`);
 });
