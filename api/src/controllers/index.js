6a23ad01f41eac8448e123fc4a9a1ca87a75b35d
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-28 14:23:29 +0700

    update route index dengan penambahan route cabang dan pesanan

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,5 +1,9 @@
 const registerController = require('./register.controller');
+const cabangController = require('./cabang.controller');
+const pesananController = require('./pesanan.controller');
 
 module.exports = {
  registerController,
+ cabangController,
+ pesananController,
 };
