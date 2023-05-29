fa861e992746b0babdd6d54a7e163c8fd7008cd0
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-30 05:39:38 +0700

    update index route dengan penambahan route ringkasan

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,12 +4,14 @@ const registerRouter = require('./register.route');
 const cabangRouter = require('./cabang.route');
 const pesananRouter = require('./pesanan.route');
 const pelangganRouter = require('./pelanggan.route');
+const ringkasanRouter = require('./ringkasan.route');
 
 router.use('/api', [
  registerRouter,
  cabangRouter,
  pesananRouter,
  pelangganRouter,
+ ringkasanRouter,
 ]);
 
 module.exports = router;
