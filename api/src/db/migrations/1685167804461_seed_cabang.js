d51ff6939fcff87fdef955b548fd902d8895c4d2
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-30 05:34:28 +0700

    update seed cabang, memperbaiki typo dan mengganti nama kota

--- api/src/db/migrations/1685167804461_seed_cabang.js
+++ api/src/db/migrations/1685167804461_seed_cabang.js
@@ -3,10 +3,10 @@ const { fakerUtils } = require('../../utils');
 module.exports = {
  up: `INSERT INTO cabang (id_pelanggan, nama_cabang, kota, kebutuhan_produk, rata_rata_kuantitas_produk) VALUES
  (1, 'Cabang Rawamangun', 'Jakarta Timur', 'Karkas,Ati ampela', '10kg'),
- (1, 'Cabang Jatinegara', 'Jakarta Timut', 'Karkas,Ati ampela', '5kg'),
+ (1, 'Cabang Jatinegara', 'Jakarta Timur', 'Karkas,Ati ampela', '5kg'),
  (2, 'Cabang Tebet', 'Jakarta Selatan', 'Karkas,Boneless', '8kg'),
  (2, 'Cabang Senen', 'Jakarta Pusat', 'Karkas,Boneless', '12kg'),
- (3, 'Cabang Dago', 'Bandung', 'Boneless', '15kg'),
+ (3, 'Cabang Matraman', 'Jakarta Timur', 'Boneless', '15kg'),
  (4, 'Cabang Dipatiukur', 'Bandung', 'Boneless,Ati ampela', '15kg'),
  (4, 'Cabang Riung', 'Bandung', 'Boneless,Ati ampela', '15kg'),
  (4, 'Cabang Cikutra', 'Bandung', 'Boneless,Ati ampela', '15kg'),
