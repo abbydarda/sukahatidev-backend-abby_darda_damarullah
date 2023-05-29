c50a04b1cbd0a793f24915d65d8d5e4bded02d28
Abby Darda Damarullah <darda.abby@gmail.com>
2023-05-30 05:32:23 +0700

    update index route dengan penambahan route ringkasan

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -2,10 +2,12 @@ const registerController = require('./register.controller');
 const cabangController = require('./cabang.controller');
 const pesananController = require('./pesanan.controller');
 const pelangganController = require('./pelanggan.controller');
+const ringkasanController = require('./ringkasan.controller');
 
 module.exports = {
  registerController,
  cabangController,
  pesananController,
  pelangganController,
+ ringkasanController,
 };
